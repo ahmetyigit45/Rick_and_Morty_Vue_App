e5f3e0c4e1b22c37ed55aac09ec9c75aee61f084
Ahmet YİĞİT <ahmetyigit92@gmail.com>
2020-12-05 22:53:40 +0300

    up

---
+++ src-cordova/src/components/Header.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-app-bar app color="#385F73" height="100">
+    <div class="d-flex align-center">
+      <v-img
+        alt="Vuetify Logo"
+        class="shrink mr-2"
+        contain
+        src="https://i.hizliresim.com/BrnyZ8.png"
+        transition="scale-transition"
+        width="100"
+      />
+    </div>
+    <v-spacer></v-spacer>
+    <v-toolbar-title style="color:white;">Rick and Morty Vue App</v-toolbar-title>
+  </v-app-bar>
+</template>
+
+<script>
+export default {
+  data() {
+    return {};
+  },
+};
+</script>
